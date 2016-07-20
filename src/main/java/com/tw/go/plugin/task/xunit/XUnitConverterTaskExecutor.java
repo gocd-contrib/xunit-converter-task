3324fc29f0636067e30d4db096bb3e860907e3f2
Aravind SV <arvind.sv@gmail.com>
2016-07-20 17:43:51 -0400

    Migrate to new communication format
    * Uses JSON now, instead of the old-style task plugin APIs.

--- src/main/java/com/tw/go/plugin/task/xunit/XUnitConverterTaskExecutor.java
+++ src/main/java/com/tw/go/plugin/task/xunit/XUnitConverterTaskExecutor.java
@@ -1,37 +1,55 @@
 package com.tw.go.plugin.task.xunit;
 
+import com.google.gson.GsonBuilder;
+import com.thoughtworks.go.plugin.api.response.DefaultGoPluginApiResponse;
+import com.thoughtworks.go.plugin.api.response.GoPluginApiResponse;
 import com.thoughtworks.go.plugin.api.response.execution.ExecutionResult;
 import com.thoughtworks.go.plugin.api.task.*;
 import com.tw.xunit.converter.TestReportConverter;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 
-public class XUnitConverterTaskExecutor implements TaskExecutor {
-    @Override
-    public ExecutionResult execute(TaskConfig config, TaskExecutionContext taskEnvironment) {
+public class XUnitConverterTaskExecutor {
+    private final String converterType;
+    private final String inputDirectory;
+    private final String outputDirectory;
+    private GsonBuilder gsonBuilder;
+
+    public XUnitConverterTaskExecutor(GsonBuilder gsonBuilder, String converterType, String inputDirectory, String outputDirectory) {
+        this.gsonBuilder = gsonBuilder;
+        this.converterType = converterType;
+        this.inputDirectory = inputDirectory;
+        this.outputDirectory = outputDirectory;
+    }
+
+    public GoPluginApiResponse execute(String workingDir) {
         try {
-            return convertToXUnit(taskEnvironment, config);
+            return convertToXUnit(workingDir);
         } catch (Exception e) {
-            return ExecutionResult.failure("Failed to download file from URL: " + config.getValue(XUnitConverterTask.CONVERTER_TYPE), e);
+            return DefaultGoPluginApiResponse.success(message(false, "Failed to download file from URL for: " + converterType + ". Error: " + e.getMessage()));
         }
     }
 
-    private ExecutionResult convertToXUnit(TaskExecutionContext taskContext, TaskConfig taskConfig) throws IOException, InterruptedException {
-        Console console = taskContext.console();
+    private GoPluginApiResponse convertToXUnit(String workingDir) throws IOException, InterruptedException {
+        JobConsoleLogger console = JobConsoleLogger.getConsoleLogger();
         console.printLine("Starting conversion...");
-
-        String converterType = taskConfig.getValue(XUnitConverterTask.CONVERTER_TYPE);
-        String inputDirectory = taskConfig.getValue(XUnitConverterTask.INPUT_DIRECTORY);
-        String outputDirectory = taskConfig.getValue(XUnitConverterTask.OUTPUT_DIRECTORY);
-
+        
         try {
-            new TestReportConverter().convert(converterType, new File(taskContext.workingDir() + "/" + inputDirectory), new File(taskContext.workingDir() + "/" + outputDirectory));
+            new TestReportConverter().convert(converterType, new File(workingDir + "/" + inputDirectory), new File(workingDir + "/" + outputDirectory));
             console.printLine("Conversion finished");
-            return ExecutionResult.success("done.");
+            return DefaultGoPluginApiResponse.success(message(true, "Done"));
         } catch (Exception e) {
             console.printLine("Failed to convert. Message: " + e.getMessage());
-            return ExecutionResult.failure("Failed to convert. Message: " + e.getMessage());
+            return DefaultGoPluginApiResponse.success(message(false, "Failed to convert. Message: " + e.getMessage()));
         }
+    }
+
+    private String message(boolean status, String message) {
+        HashMap<String, Object> result = new HashMap<String, Object>();
+        result.put("success", status);
+        result.put("message", message);
+        return gsonBuilder.create().toJson(result);
     }
 }
